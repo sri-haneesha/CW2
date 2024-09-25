a0806c958637d6371bd6000e103610875f20d8e4
sri-haneesha <152427868+sri-haneesha@users.noreply.github.com>
2024-09-24 23:09:31 -0400

    Update FavoritesScreen.dart

--- lib/FavoritesScreen.dart
+++ lib/FavoritesScreen.dart
@@ -9,15 +9,24 @@ class FavoritesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Favorite Recipes'),
+        title: Text('Favorite Recipes', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.red,
       ),
       body: favoriteRecipes.isEmpty
-          ? Center(child: Text('No favorites yet!'))
+          ? Center(
+              child: Text(
+                'No favorites yet!',
+                style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+              ),
+            )
           : ListView.builder(
               itemCount: favoriteRecipes.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text(favoriteRecipes[index]),
+                  title: Text(
+                    favoriteRecipes[index],
+                    style: TextStyle(fontSize: 18, color: Colors.black87),
+                  ),
                 );
               },
             ),
