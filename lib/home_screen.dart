01cd3aa041410eb884c5bbe8f6144bca54a0c7e8
sri-haneesha <152427868+sri-haneesha@users.noreply.github.com>
2024-09-24 23:10:18 -0400

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'details_screen.dart';
-import 'FavoritesScreen.dart';
+import 'favorites_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -17,18 +17,18 @@ class _HomeScreenState extends State<HomeScreen> {
         'Egg',
         'Pepper',
         'Parmigiano Reggiano',
-        'Carbonara is a classic, 5-ingredient Roman pasta dish made with pasta, eggs, guanciale, Pecorino Romano cheese and black pepper. It’s made by tempering eggs, cheese and starchy pasta cooking water into a luscious creamy pasta sauce. The taste is rich, salty and addictive! There is absolutely no heavy cream in traditional carbonara sauce.'
+        'Carbonara is a classic, 5-ingredient Roman pasta dish made with pasta, eggs, guanciale, Pecorino Romano cheese and black pepper.'
       ],
     },
     {
       'name': 'Pizza',
       'Ingredients': [
         'Dough',
-        'Favourite Sauce',
-        'Favourite Cheese',
+        'Favorite Sauce',
+        'Favorite Cheese',
         'Olive Oil',
         'Oregano',
-        'Lets dive into the art of homemade pizza dough! It may seem like a lot of work, but the results promise a delicious meal and the satisfaction of mastering a kitchen staple. With just a handful of ingredients and a bit of patience for rising and resting, you'll soon be on your way to creating a pizza crust that's crispy on the outside and delightfully chewy on the inside.'
+        'Lets dive into the art of homemade pizza dough!'
       ],
     },
     {
@@ -39,7 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
         'Milk',
         'Cheese',
         'Salt & Pepper',
-        'The BEST Homemade Mac and Cheese of your LIFE. Outrageously cheesy, ultra creamy, and topped with a crunchy Panko-Parmesan topping, this mac and cheese recipe is most definitely a keeper.'
+        'The BEST Homemade Mac and Cheese of your LIFE.'
       ],
     },
   ];
@@ -50,46 +50,53 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Recipe List'),
+        title: Text('Recipe List', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.red,
       ),
       body: ListView.builder(
         itemCount: recipes.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(recipes[index]['name']),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailsScreen(
-                    recipe: recipes[index]['name'],
-                    ingredients: recipes[index]['ingredients'],
-                    onFavoriteToggle: (isFavorite) {
-                      setState(() {
-                        if (isFavorite) {
-                          favoriteRecipes.add(recipes[index]['name']);
-                        } else {
-                          favoriteRecipes.remove(recipes[index]['name']);
-                        }
-                      });
-                    },
-                    isFavorite:
-                        favoriteRecipes.contains(recipes[index]['name']),
+          return Card(
+            elevation: 2,
+            margin: EdgeInsets.all(8.0),
+            child: ListTile(
+              title: Text(
+                recipes[index]['name'],
+                style: TextStyle(fontSize: 20, color: Colors.black87),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailsScreen(
+                      recipe: recipes[index]['name'],
+                      ingredients: recipes[index]['Ingredients'],
+                      onFavoriteToggle: (isFavorite) {
+                        setState(() {
+                          if (isFavorite) {
+                            favoriteRecipes.add(recipes[index]['name']);
+                          } else {
+                            favoriteRecipes.remove(recipes[index]['name']);
+                          }
+                        });
+                      },
+                      isFavorite: favoriteRecipes.contains(recipes[index]['name']),
+                    ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.favorite),
+        backgroundColor: Colors.red,
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) =>
-                  FavoritesScreen(favoriteRecipes: favoriteRecipes),
+              builder: (context) => FavoritesScreen(favoriteRecipes: favoriteRecipes),
             ),
           );
         },
